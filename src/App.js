708dbe9cdab686ee05a8bddb4f79dc6a4455a8da
Aswany <aswany.mail@gmail.com>
2022-04-26 20:35:32 +0800

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [stocks, setStock] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/api/stocks`)
+      .then(response => response.json())
+      .then(data => {
+
+        setStock(data.map(d => {
+          return d.result
+        }))
+
+      })
+
+  }, []);
+
+  const reduceName = (name) => { return name && name.length > 17 ? `${name.substring(0, 17)}...` : name };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div>
+          {
+            stocks.map(data =>
+              <div className="tableContainer" >
+                <div className="image">
+                  <img src={data.logo} alt="Logo" className="icon" />
+                </div>
+
+                <div className="rateContainer">
+                  <div className="tableTitle">{data.symbol}</div>
+                  <div className="tableColumn">{reduceName(data.companyName)}</div>
+                </div>
+
+                <div className="rateContainer">
+                  <div className="stockPrice">${data.previousClose}</div>
+                  <div className="progressContainer">
+                    <div className="dollarValue">{data.change}</div>
+                    <div className="percentageChange">{data.changePercent}%</div>
+                  </div>
+                </div>
+
+              </div>
+            )}
+        </div>
+      </div>
     </div>
   );
 }
